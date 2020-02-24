c98b1158f4b255fdbc028e5a9808ad1a174c40f8
RunawayTatti <tatjanawitt@gmx.de>
2020-02-24 22:05:53 +0100

    wip

--- app.js
+++ app.js
@@ -9,17 +9,20 @@ const cors = require('cors');
 const routes = require('./api/routes');
 
 const app = express();
-//const client = 'jquery'     // JS Kurs 1
-//const client = 'vue'        // JS Kurs 2
-const client = 'angular/src'  // JS Kurs 3
 
 app.use(cors());
 app.use(morgan('dev', {  //control api status
     skip: (req, res) => res.statusCode < 1
 }));
+
+let client = 'angular/src'; // node app.js j | v | a
+if(process.argv[2] == 'j') client = 'jquery';       // JS Kurs 1
+if(process.argv[2] == 'v') client = 'vue';          // JS Kurs 2
+if(process.argv[2] == 'a') client = 'angular/src';  // JS Kurs 3
 app.use( express.static ('webclient_' + client , {
     extensions: ['html', 'htm'],
 }));
+
 app.use(bodyParser.json());
 app.use(nocache());
 app.use('/api', routes); // Präfix beim Routing
